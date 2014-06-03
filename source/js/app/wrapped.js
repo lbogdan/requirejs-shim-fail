c1969e96fa909f1018e477d186b0e0d3e2084558
Makara Sok <makara.aktee.sok@gmail.com>
2014-06-03 14:02:33 +0700

    initial

---
+++ source/js/app/wrapped.js
@@ -0,0 +1,7 @@
+define(function(require) {
+    var $ = require('jquery');
+
+    require('vendor/magnific-popup/magnific-popup-wrapped');
+
+    $('.magnificpopup').magnificPopup({type:'image'})
+});
---
+++ source/js/shim-only.js
@@ -0,0 +1,7 @@
+/*
+ * This acts like a routing file. All it does is simply make it require the common config file
+ * and loads the proper app script.
+*/
+require(['common'], function () {
+    require(['app/shim-only']);
+});
